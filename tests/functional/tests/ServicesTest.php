2c46cf3246803c999b6935ee75f722e77b461447
Marat Fakhertdinov <marat.fakhertdinov@gmail.com>
2014-10-25 14:24:05 +0700

    Added functional tests

---
+++ tests/functional/tests/ServicesTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Butterfly\Tests;
+
+class ServicesTest extends BaseDiTest
+{
+    protected static function getAdditionalConfigPaths()
+    {
+        return array(
+            self::$baseDir . '/config/test.yml',
+        );
+    }
+
+
+    public function getDataForTestService()
+    {
+        return array(
+            array('bfy_adapter.http_foundation.session'),
+            array('bfy_adapter.http_foundation.request'),
+
+            array('bfy_app.request_response.request_handler.default'),
+            array('bfy_app.request_response'),
+        );
+    }
+
+    /**
+     * @dataProvider getDataForTestService
+     * @param string $serviceName
+     */
+    public function testService($serviceName)
+    {
+        self::$container->get($serviceName);
+    }
+}
